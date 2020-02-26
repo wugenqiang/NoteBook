92b2443d3f89c83406fcf33f7ebbc231f4fac7cc
WuGenQiang <2422676183@qq.com>
2020-02-26 21:48:02 +0800

    增加 技术笔记

--- C/code/test.c
+++ C/code/test.c
@@ -1,20 +1,21 @@
-#include "stdio.h" 
-/*计算时间差*/ 
-int main()
-{
-	int hour1, minute1;
-	int hour2, minute2;
-	
-	scanf("%d %d", &hour1, &minute1);
-	scanf("%d %d", &hour2, &minute2);
-	
-	int t1 = hour1 * 60 + minute1;
-	int t2 = hour2 * 60 + minute2;
-	
-	int t = t2 - t1;
-	
-	printf("时间差是 %d 小时 %d 分钟。",t/60, t%60); 
-	
-	return 0; 
+#include "stdio.h"
+ 
+int recursion_fun(int n){
+    if(n == 0) return 1;
+	if(n == 1) return 1;//递归结束条件
+	else	return n * recursion_fun( n-1 );  //自己调用自己
 }
  
+//主函数 
+int main(){
+	int n,res;
+	 
+	printf("请输入递归数字：");
+	scanf("%d",&n);
+	
+	res = recursion_fun(n);			//n! 
+	
+	printf("\n %d! = %d",n,res);
+	return 0;
+}
+
