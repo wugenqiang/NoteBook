4b71334fdfea59792a0c49b37c4f384d4197b62e
WuGenQiang <2422676183@qq.com>
2020-03-16 21:31:29 +0800

    add 回到顶部 功能

--- C/C-Code.md
+++ C/C-Code.md
@@ -844,3 +844,28 @@ int main(){
 
 【代码】
 
+```c
+#include <stdio.h>
+#include <string.h> 
+
+int main(){
+	char str[3][20];	//定义二维字符数组 
+	char string[20];	//定义一维字符数组，作为交换字符串时候的临时字符数组	 
+	int i;
+	for(i=0;i<3;i++){
+		gets(str[i]);
+	}
+	if(strcmp(str[0],str[1])>0){
+		strcpy(string,str[0]);
+	}else{
+		strcpy(string,str[1]);
+	}
+	if(strcmp(str[2],string)>0){
+		strcpy(string,str[2]);
+	}
+	printf("\nThe largest string is：%s\n",string);
+	
+	return 0;
+}
+```
+
--- C/code/test.c
+++ C/code/test.c
@@ -1,19 +1,22 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h> 
 
 int main(){
-	char string[81];
-	int i,num = 0,word = 0;
-	char c;
-	gets(string);
-	for(i=0;(c=string[i])!='\0';i++){
-		if(c == ' '){
-			word = 0;
-		}else if(word == 0){
-			word = 1;
-			num++;
-		}
+	char str[3][20];	//定义二维字符数组 
+	char string[20];	//定义一维字符数组，作为交换字符串时候的临时字符数组	 
+	int i;
+	for(i=0;i<3;i++){
+		gets(str[i]);
 	}
-	printf("There are %d words in this line.\n",num);
+	if(strcmp(str[0],str[1])>0){
+		strcpy(string,str[0]);
+	}else{
+		strcpy(string,str[1]);
+	}
+	if(strcmp(str[2],string)>0){
+		strcpy(string,str[2]);
+	}
+	printf("%s\n",string);
 	
 	return 0;
 }
