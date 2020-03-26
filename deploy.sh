efe3c3b9cdce0477bc10a078192d96f4c778fc9c
WuGenQiang <2422676183@qq.com>
2020-03-26 14:26:25 +0800

    auto update https://wugenqiang.github.io/CS-Notes

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,13 @@ git commit -m 'auto update https://wugenqiang.github.io/CS-Notes'
 echo '--------commit successfully--------'
 
 # git push -f https://github.com/wugenqiang/CS-Notes.git master
-git push -u https://github.com/wugenqiang/CS-Notes.git origin master
+git push -u origin master https://github.com/wugenqiang/CS-Notes.git
 # git remote add origin https://github.com/wugenqiang/CS-Notes.git
 # git push -u origin master
 echo '--------push to GitHub successfully--------'
 
 # git push -f https://gitee.com/wugenqiang/CS-Notes.git master
-git push -u https://gitee.com/wugenqiang/CS-Notes.git origin master
+git push -u origin master https://gitee.com/wugenqiang/CS-Notes.git
 # git remote add origin https://gitee.com/wugenqiang/CS-Notes.git
 # git push -u origin master
 echo '--------push to Gitee successfully--------'
