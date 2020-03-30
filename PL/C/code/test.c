9e55f94da29fb69c1edf3cfd3b196ccf844e332d
WuGenQiang <2422676183@qq.com>
2020-03-30 21:20:40 +0800

    auto update https://wugenqiang.github.io/CS-Notes

--- PL/C/code/test.c
+++ PL/C/code/test.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-void PrintReverseString()
-{
-    char c;
-    scanf("%c", &c);
-    if ('\n' == c)
-        return;
-    else
-    {
-        PrintReverseString();
-        printf("%c", c);
-    }
-
-}
-
-int main()
-{
-    PrintReverseString();     
-    return 0;
+int main(){
+	char s[105];
+	gets(s);
+	int i;
+	for(i = 0; i < strlen(s); i++){
+		if(s[i] >= 'A' && s[i] <= 'Z'){
+			s[i] += 3;
+			if(s[i] > 'Z'){
+				s[i] -= 26;//溢出循环 
+			}
+		}else if(s[i] >= 'a' && s[i] <= 'z'){
+			s[i] += 3;
+			if(s[i] > 'z'){
+				s[i] -= 26;//溢出循环 
+			}
+		} else{
+			continue;
+		} 
+	}
+	puts(s);
+	return 0;
 }
--- PL/C/code/test2.cpp
+++ PL/C/code/test2.cpp
@@ -1,25 +1,33 @@
 #include <bits/stdc++.h> //万能头文件 
 using namespace std;
+
+struct node{
+	string num;
+	string name;
+	string sex;
+	int age;
+};
+
 int main(){
-	double a;
-	scanf("%lf",&a);
-	//使用%g可以自动去掉小数点后多余的0，如果是整数则显示整数
-	if (a<1000){
-		printf("discount=1,pay=%g\n",a);
+	int n,q;
+	map<string,node> M;//定义一个map映射
+	while(scanf("%d",&n)!=EOF){
+		for(int i=0;i<n;i++){
+			node tmp;
+			cin>>tmp.num>>tmp.name>>tmp.sex>>tmp.age;
+			M[tmp.num]=tmp;//将学号指向对应的结构体 
+		}
+		scanf("%d",&q);
+		for(int i=0;i<q;i++){
+			string num;
+			cin>>num;
+			if((M.find(num))!=M.end()){//find查找
+				cout<<M[num].num<<" "<<M[num].name<<M[num].sex<<" "<<M[num].age<<endl; 	
+			}else{
+				cout<<"No Answer!"<<endl;
+			}
+		} 
 	} 
-	if (a>=1000 && a<2000){
-		printf("discount=0.95,pay=%g\n",a*0.95);
-	}
-	if (a>=2000 && a<3000){
-		printf("discount=0.9,pay=%g\n",a*0.9);
-	}  
-	if (a>=3000 && a<5000){
-		printf("discount=0.85,pay=%g\n",a*0.85);
-	}  
-	if (a>=5000){
-		printf("discount=0.8,pay=%g\n",a*0.8);
-	} 	
-	
 	return 0;
 }
 
