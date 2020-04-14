f8ddedb47c66af42a584354d29de5e7a36453a03
WuGenQiang <2422676183@qq.com>
2020-04-14 12:01:41 +0800

    auto update https://wugenqiang.github.io/CS-Notes

--- PL/C/code/test.c
+++ PL/C/code/test.c
@@ -1,31 +1,48 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-void fun(char *tt, int pp[]){
-	int i, j = 0;
-	for (i = 0; i < strlen(tt); i++){
-		if (tt[i] >= 'a' && tt[i] <= 'z'){
-			j = tt[i] - 'a' ;
-			pp[j] = pp[j] + 1;
-		}
-	}
+int BinarySearch(int *array, int key, int low, int high)
+{
+    int mid;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (key == array[mid])
+        {
+            return mid;
+        }
+        else if (key < array[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return 0;
 }
 
-int main(){//main function is used to Test fun function
-	char tt[] = {'a','b','a'}; 
-	int i;
-	int pp[26];
-	//int pp[26] = {0};
-	for (i = 0; i < 26; i++){
-		pp[i] = 0;
-	} 
-	fun(&tt, pp);
-	
-	for (i = 0; pp[i] != '\0'; i++){
-		if (pp[i] > 0){
-			char c = i + 'a';
-			printf("%c = %d\n", c, pp[i]);
-		}
-	}	
-	return 0;
+int main()
+{
+    int n, i, key, position;
+    int *array;
+    printf("请输入有序数组的大小：");
+    scanf("%d", &n);
+    array = (int*) malloc(sizeof(int) * n);
+    printf("请按升序输入数据：\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("请输入想要查找的数：");
+    scanf("%d", &key);
+    if (position = BinarySearch(array, key, 0, n - 1)) 
+    {
+        printf("%d的位置为：%d\n", key, position);
+    }
+    else
+    {
+        printf("%d不存在\n", key);    
+    }
 }
